c34885ab2a8fde39a2a3864060d8b5ba899eddc1
22.jakub@gmail.com <22.jakub@gmail.com>
2015-04-25 16:34:46 +0200

    ilumination about drawing

---
+++ backgroundFullfillment.asm
@@ -0,0 +1,24 @@
+#fill background
+la      $t1, ($t0)
+add	$t2, $t1, $t5
+li      $t3, 0xff
+
+li	$t7, 0			#"y" location on inmage
+lw	$t8, bw
+lw	$t9, bh
+mul	$t8, $t8, 3
+mul	$t9, $t9, 3
+
+fill_loop:
+li	$t6, 0			#"x" location on image
+
+fill_row_loop:
+#filling row
+sb      $t3, ($t1)
+addi    $t1, $t1, 1
+addi	$t6, $t6, 1
+blt	$t6, $t8, fill_row_loop
+
+#next row
+addi	$t7, $t7, 1
+blt     $t7, $t9, fill_loop
--- main.asm
+++ main.asm
@@ -43,6 +43,8 @@ bh:             .word 0     # biHeight
 #bitmap_end:     .word 0
 
 .text
+#################################################
+#getting input starts here
 
 #!!!
 #WKLEIC TU POTEM:
@@ -99,30 +101,9 @@ move	$a0, $t5
 syscall
 move	$t0, $v0
 
-#fill background
-la      $t1, ($t0)
-add	$t2, $t1, $t5
-li      $t3, 0xff
+#################################################
+#painting starts here
 
-li	$t7, 0			#"y" location on inmage
-lw	$t8, bw
-lw	$t9, bh
-mul	$t8, $t8, 3
-mul	$t9, $t9, 3
-
-fill_loop:
-li	$t6, 0			#"x" location on image
-
-fill_row_loop:
-#filling row
-sb      $t3, ($t1)
-addi    $t1, $t1, 1
-addi	$t6, $t6, 1
-blt	$t6, $t8, fill_row_loop
-
-#next row
-addi	$t7, $t7, 1
-blt     $t7, $t9, fill_loop
 
 #save bitmap from buffer to file
 li      $v0, 13			#open file
