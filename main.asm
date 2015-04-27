8baa68652db92a8bd8344519a3114281eb22f833
22.jakub@gmail.com <22.jakub@gmail.com>
2015-04-28 01:27:12 +0200

    final

--- main.asm
+++ main.asm
@@ -9,7 +9,7 @@ output:		.asciiz "out.bmp"
 
 width_prompt:	.asciiz "podaj szerokosc: "
 height_prompt:	.asciiz "\npodaj wysokosc: "
-points_prompt:	.asciiz "\npodaj 5 punktow kotrolnych (wartosci procentowe):"
+points_prompt:	.asciiz "\npodaj 5 punktow kotrolnych (fixed point 16,16):"
 x_prompt:	.asciiz "\n\nx: "
 y_prompt:	.asciiz "\ny: "
 debug:		.asciiz "\n-------------------------------------\n"
@@ -61,43 +61,122 @@ bh:             .word 0     # biHeight
 #getting input starts here
 
 #!!!
-#WKLEIC TU POTEM:
 #readInput.asm
-#!!!
-
-#!!!
-#WYWALIC TO POTEM:
-#hardcoded, converted to fixed binary values for testing
+#gathering resolution information
 #t1 - width
 #t2 - height
-li	$t1, 520
-li	$t2, 380
+li      $v0, 4			#print string
+la      $a0, width_prompt
+syscall
 
+li	$v0, 5			#read int
+syscall
+move	$t1, $v0
+
+li      $v0, 4			#print string
+la      $a0, height_prompt
+syscall
+
+li	$v0, 5			#read int
+syscall
+move	$t2, $v0
+
+#gathering control points information
+#t3 - array pointer
 la	$t3, control_points
-li	$t0, 0x1999
-sw	$t0, ($t3)
-li	$t0, 0x1999
-sw	$t0, 4($t3)
 
-li	$t0, 0xe666
-sw	$t0, 8($t3)
-li	$t0, 0x1999
-sw	$t0, 12($t3)
+li      $v0, 4			#print string
+la      $a0, points_prompt
+syscall
 
-li	$t0, 0x1999
-sw	$t0, 16($t3)
-li	$t0, 0xe666
-sw	$t0, 20($t3)
+#1st point
+li      $v0, 4			#print string
+la      $a0, x_prompt
+syscall
 
-li	$t0, 0x8000
-sw	$t0, 24($t3)
-li	$t0, 0xe666
-sw	$t0, 28($t3)
+li	$v0, 5			#read int
+syscall
+sw	$v0, ($t3)
 
-li	$t0, 0xe666
-sw	$t0, 32($t3)
-li	$t0, 0xe666
-sw	$t0, 36($t3)
+li      $v0, 4			#print string
+la      $a0, y_prompt
+syscall
+
+li	$v0, 5			#read int
+syscall
+sw	$v0, 4($t3)
+
+#2nd point
+li      $v0, 4			#print string
+la      $a0, x_prompt
+syscall
+
+li	$v0, 5			#read int
+syscall
+sw	$v0, 8($t3)
+
+li      $v0, 4			#print string
+la      $a0, y_prompt
+syscall
+
+li	$v0, 5			#read int
+syscall
+sw	$v0, 12($t3)
+
+#3rd point
+li      $v0, 4			#print string
+la      $a0, x_prompt
+syscall
+
+li	$v0, 5			#read int
+syscall
+sw	$v0, 16($t3)
+
+li      $v0, 4			#print string
+la      $a0, y_prompt
+syscall
+
+li	$v0, 5			#read int
+syscall
+sw	$v0, 20($t3)
+
+#4th point
+li      $v0, 4			#print string
+la      $a0, x_prompt
+syscall
+
+li	$v0, 5			#read int
+syscall
+sw	$v0, 24($t3)
+
+li      $v0, 4			#print string
+la      $a0, y_prompt
+syscall
+
+li	$v0, 5			#read int
+syscall
+sw	$v0, 28($t3)
+
+#5th point
+li      $v0, 4			#print string
+la      $a0, x_prompt
+syscall
+
+li	$v0, 5			#read int
+syscall
+sw	$v0, 32($t3)
+
+li      $v0, 4			#print string
+la      $a0, y_prompt
+syscall
+
+li	$v0, 5			#read int
+syscall
+sw	$v0, 36($t3)
+#!!!
+
+#!!!
+#hardcoded, converted to fixed binary values for testing can be put here
 #!!!
 
 #assighning correct size
@@ -117,12 +196,23 @@ sw	$t4, bitmap_size
 li	$v0, 9			#sbrk
 move	$a0, $t5
 syscall
-sb	$v0, bm_data_start
+sw	$v0, bm_data_start
 
 #################################################
 #painting starts here
 
-li	$t0, 0x200
+#fill background
+lw 	$t1, bm_data_start
+add	$t2, $t1, $t5
+li	$t3, 0x0
+
+fill_next_byte:
+sb	 $t3, ($t1)
+addi	 $t1, $t1, 1
+blt	 $t1, $t2, fill_next_byte
+
+#beginning of casteljau algorithm
+li	$t0, 0x80
 casteljau_loop:
 li	$t2, 0x10000
 sub	$t1, $t2, $t0
@@ -183,19 +273,6 @@ mul	$s1, $s1, $t0
 srl	$s1, $s1, 16
 add	$t9, $t9, $s1			#"13"
 
-li $v0,4
-la $a0,x_prompt
-syscall
-li $v0,1
-move $a0,$t4
-syscall
-li $v0,4
-la $a0,y_prompt
-syscall
-li $v0,1
-move $a0,$t3
-syscall
-
 #2nd column
 mul	$t2, $t2, $t1
 srl	$t2, $t2, 16
@@ -230,24 +307,6 @@ mul	$s1, $t9, $t0
 srl	$s1, $s1, 16
 add	$t7, $t7, $s1			#"22"
 
-
-
-li $v0,4
-la $a0,x_prompt
-syscall
-li $v0,1
-move $a0,$t4
-syscall
-li $v0,4
-la $a0,y_prompt
-syscall
-li $v0,1
-move $a0,$t3
-syscall
-
-
-
-
 #3rd column
 mul	$t2, $t2, $t1
 srl	$t2, $t2, 16
@@ -271,25 +330,6 @@ mul	$s0, $t7, $t0
 srl	$s0, $s0, 16
 add	$t5, $t5, $s0			#"31"
 
-
-
-
-li $v0,4
-la $a0,x_prompt
-syscall
-li $v0,1
-move $a0,$t4
-syscall
-li $v0,4
-la $a0,y_prompt
-syscall
-li $v0,1
-move $a0,$t3
-syscall
-
-
-
-
 #4th column
 mul	$t2, $t2, $t1
 srl	$t2, $t2, 16
@@ -301,25 +341,6 @@ srl	$t3, $t3, 16
 mul	$t5, $t5, $t0
 srl	$t5, $t5, 16
 add	$t3, $t3, $t5			#"40" - final point
-
-
-
-li $v0,4
-la $a0,x_prompt
-syscall
-li $v0,1
-move $a0,$t4
-syscall
-li $v0,4
-la $a0,y_prompt
-syscall
-li $v0,1
-move $a0,$t3
-syscall
-
-
-
-
 
 #drawing point
 # (x0, y0) =
@@ -334,7 +355,7 @@ srl	$t2, $t2, 16
 mul	$t3, $t3, $t5			#y0
 srl	$t3, $t3, 16
 
-la	$t6, bm_data_start		#addr
+lw	$t6, bm_data_start		#addr
 lw	$t4, triple_w			#3 * width
 subi	$t3, $t3, 1			#y0 - 1
 mul	$t3, $t3, $t4			#3 * width * (y0 - 1)
@@ -343,17 +364,18 @@ add	$t2, $t7, $t2			#3 * x0
 add	$t6, $t6, $t3
 add	$t6, $t6, $t2
 
-li      $t3, 0xff			#putting pixel
+li      $t3, 0x0			#putting pixel
 sb	$t3, ($t6)
 addi	$t6, $t6, 1
-li      $t3, 0xff
+li      $t3, 0x0
 sb	$t3, ($t6)
 addi	$t6, $t6, 1
-li      $t3, 0xff
+li      $t3, 0xef
 sb	$t3, ($t6)
 
-addi	$t0, $t0, 0x200
+addi	$t0, $t0, 0x80
 blt	$t0, 0x10000, casteljau_loop
+
 
 #################################################
 #saving stuff starts here
@@ -375,7 +397,7 @@ syscall
 #write pixels
 li      $v0, 15			#write to file
 move    $a0, $t1		#file desc
-la      $a1, bm_data_start	#bitmap buffer
+lw      $a1, bm_data_start	#bitmap buffer
 lw	$t5, bitmap_size
 subi	$t5, $t5, 54
 move    $a2, $t5		#buffer length
--- set4.asm
+++ set4.asm
@@ -4,27 +4,27 @@ li	$t1, 520
 li	$t2, 380
 
 la	$t3, control_points
-li	$t0, 0xe666
+li	$t0, 0x8000
 sw	$t0, ($t3)
-li	$t0, 0x1999
+li	$t0, 0xe666
 sw	$t0, 4($t3)
 
 li	$t0, 0x1999
 sw	$t0, 8($t3)
-li	$t0, 0xe666
+li	$t0, 0x1999
 sw	$t0, 12($t3)
 
-li	$t0, 0x8000
+li	$t0, 0xe666
 sw	$t0, 16($t3)
 li	$t0, 0xe666
 sw	$t0, 20($t3)
 
-li	$t0, 0xe666
+li	$t0, 0x1999
 sw	$t0, 24($t3)
 li	$t0, 0xe666
 sw	$t0, 28($t3)
 
-li	$t0, 0x1999
+li	$t0, 0xe666
 sw	$t0, 32($t3)
 li	$t0, 0x1999
 sw	$t0, 36($t3)
